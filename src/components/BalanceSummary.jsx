171439d703e5194d2bf6eca8e78ba2fe1dba2294
Egar19 <madeeegar@gmail.com>
2025-07-23 11:10:10 +0800

    Adjust stat-value font size for better responsiveness on smaller screens

--- src/components/BalanceSummary.jsx
+++ src/components/BalanceSummary.jsx
@@ -5,17 +5,17 @@ const BalanceSUmmary = ({ balance, totalIncome, totalOutcome }) => {
     <div className='stats stats-vertical lg:stats-horizontal shadow bg-base-200 w-full'>
       <div className='stat'>
         <div className='stat-title'>Balance: </div>
-        <div className='stat-value'>{formatRupiah(balance)}</div>
+        <div className='stat-value sm:text-2xl text-xl lg:text-4xl'>{formatRupiah(balance)}</div>
       </div>
 
       <div className='stat'>
         <div className='stat-title'>Total Income</div>
-        <div className='stat-value'>{formatRupiah(totalIncome)}</div>
+        <div className='stat-value sm:text-2xl text-xl lg:text-4xl'>{formatRupiah(totalIncome)}</div>
       </div>
 
       <div className='stat'>
         <div className='stat-title'>Total outcome</div>
-        <div className='stat-value'>{formatRupiah(totalOutcome)}</div>
+        <div className='stat-value sm:text-2xl text-xl lg:text-4xl'>{formatRupiah(totalOutcome)}</div>
       </div>
     </div>
   );
